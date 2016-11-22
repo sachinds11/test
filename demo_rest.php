3d6bc4214126e3c511e9f689c8e4bd9f10335bd7
unknown <admin@admin.com>
2016-11-22 15:06:30 +0530

    save

--- demo_rest.php
+++ demo_rest.php
@@ -6,4 +6,32 @@ $instance_url = $_SESSION['instance_url'];
 echo "access token ::". $access_token. "<br>";
 echo "instance_url ::". $instance_url. "<br>";
 
+function show_accounts($instance_url, $access_token) {
+    $query = "SELECT Name, Id from Account LIMIT 100";
+    $url = "$instance_url/services/data/v20.0/query?q=" . urlencode($query);
+
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER,
+            array("Authorization: OAuth $access_token"));
+
+    $json_response = curl_exec($curl);
+    curl_close($curl);
+
+    $response = json_decode($json_response, true);
+
+    $total_size = $response['totalSize'];
+
+    echo "$total_size record(s) returned<br/><br/>";
+    foreach ((array) $response['records'] as $record) {
+        echo $record['Id'] . ", " . $record['Name'] . "<br/>";
+    }
+    echo "<br/>";
+}
+
+
+show_accounts($instance_url, $access_token);
+
+
 
