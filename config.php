2b9b2120a39cc59f0f4888b5f6c05df99b1b2f42
unknown <admin@admin.com>
2016-11-22 14:34:53 +0530

    3 commit

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define("CLIENT_ID", "3MVG9ZL0ppGP5UrBAVbNvoDXanSXhKC2ptOA5_PhZ9dfXVmxD5TlFpud1xcncj4bKdubR_nKD1Hy_aS8icQjP");
+define("CLIENT_SECRET", "4695938263465200539");
+define("REDIRECT_URI", "https://ps12.herokuapp.com/oauth_callback.php");
+define("LOGIN_URI", "https://login.salesforce.com");
+?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-echo "hello";
+//echo "hello";
 
-echo "test1";
+//echo "test1";
 $ch = curl_init();  
  
  $url="https://login.salesforce.com/services/oauth2/token";
@@ -10,7 +10,7 @@ $ch = curl_init();
  curl_setopt($ch,CURLOPT_URL,$url);
     //curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);	
-curl_setopt($ch, CURLOPT_POSTFIELDS, array('grant_type' => 'password', 'client_id' => '3MVG9ZL0ppGP5UrBAVbNvoDXanSXhKC2ptOA5_PhZ9dfXVmxD5TlFpud1xcncj4bKdubR_nKD1Hy_aS8icQjP','client_secret' => '4695938263465200539','username' => 'manoj_kmwt@hotmail.com','password' => 'tiya1122','redirect_uri' =>'http://localhost/sd/index.php'));	
+curl_setopt($ch, CURLOPT_POSTFIELDS, array('grant_type' => 'password', 'client_id' => '3MVG9ZL0ppGP5UrBAVbNvoDXanSXhKC2ptOA5_PhZ9dfXVmxD5TlFpud1xcncj4bKdubR_nKD1Hy_aS8icQjP','client_secret' => '4695938263465200539','username' => 'manoj_kmwt@hotmail.com','password' => 'tiya1122','redirect_uri' =>'https://ps12.herokuapp.com/index.php'));	
   //curl_setopt($ch,CURLOPT_HEADER, false); 
  
     $output=curl_exec($ch);
